f5215d7d0cdb365415b19fea4fe61c53e0a229d0
Julian Derungs <julian.derungs@deepbase.ch>
2022-07-10 02:41:06 +0200

    test: adjust tests

--- test/e2e/blok.spec.js
+++ test/e2e/blok.spec.js
@@ -1,24 +1,31 @@
-import { setupTest, createPage } from "@nuxt/test-utils";
+import { fileURLToPath } from 'node:url'
+import { describe, expect, it } from 'vitest'
+import { $fetch, setup } from '@nuxt/test-utils-edge'
 
-describe("Blok", () => {
-  setupTest({
-    browser: true,
-  });
+const playground = fileURLToPath(new URL('../../playground', import.meta.url))
 
-  it("loads bloks dynamically", async () => {
-    const page = await createPage("/");
-    const html = await page.innerHTML("body");
+await setup({
+  rootDir: playground,
+  server: true,
+  browser: false
+})
 
-    expect(html).toContain("This is totally foo!");
-    expect(html).toContain("This is a bar");
-    expect(html).toContain("This bar is somewhat different");
-    expect(html).toContain("Another bar hanging with the cool kids");
-  });
+describe('Blok component', () => {
+  it('should render the index page', async () => {
+    const html = await $fetch('/')
+    expect(html).toContain('<h1>index</h1>')
+  }, 50000)
 
-  it("handles scoped slotData correctly", async () => {
-    const page = await createPage("/");
-    const html = await page.innerHTML("body");
+  it('loads bloks dynamically', async () => {
+    const html = await $fetch('/')
+    expect(html).toContain('This is totally foo!')
+    expect(html).toContain('This is a bar')
+    expect(html).toContain('This bar is somewhat different')
+    expect(html).toContain('Another bar hanging with the cool kids')
+  }, 50000)
 
-    expect(html).toContain("vue");
-  });
-});
+  it('handles scoped slotData correctly', async () => {
+    const html = await $fetch('/')
+    expect(html).toContain('vue')
+  }, 50000)
+})
--- test/fixture/nuxt.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  components: [
-    { path: "~/components/awesome", prefix: "awesome", pathPrefix: false },
-  ],
-  modules: [["../../src/module.js", { withConsole: true }]],
-};
